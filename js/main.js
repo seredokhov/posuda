32873ad529e0e66ae7e7a053d7299e49fcece526
seredokhov <seredokhovp@mail.ru>
2017-11-13 19:57:37 +0200

    Add 3 pages

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 
 $(function(){
 	$('#owl-demo').owlCarousel({
-		navigation : false, // Show next and prev buttons
+		navigation : false,
 		slideSpeed : 300,
 		paginationSpeed : 400,
 		transitionStyle:'fadeUp',
@@ -69,7 +69,7 @@ $(function(){
 /* Каталог */
 
 $(function(){
-	if($(document).width() < 992) {
+	if($(document).width() < 364) {
 		var openLink = $('.catalog').find('.aside_header');
 		var ul = $('.cat_list');
 
@@ -91,3 +91,116 @@ $(function(){
 		});
 	}
 });
+
+/* Категории */
+$(function(){
+	var ul = $('.cat_block').find('ul');
+	var li = ul.find('li');
+	var link = li.find('a');
+	var btn = $('.mobile_cat').find('button');
+
+	if($(document).width() > 767) {
+		link.click(function(){
+
+			li.not($(this).parent()).removeClass('active');
+			$(this).parent().addClass('active');
+			return false;
+
+		})
+	}
+	else {
+		link.click(function(){
+
+			var txt = $(this).text();
+
+			li.not($(this).parent()).removeClass('active');
+			$(this).parent().addClass('active');
+			btn.text(txt);
+			ul.slideUp();
+
+			return false;
+
+		})
+	}
+
+	btn.click(function(){
+		ul.slideToggle();
+	})
+
+});
+
+
+
+/* Фильтр (селекты) */
+$(function(){
+	var select = $('.filter_block').find('.select');
+	var panel = select.find('.panel');
+	var ul = select.find('.drop_select');
+	var li = ul.find('li');
+
+	panel.click(function(){
+		ul.not($(this).siblings('ul')).slideUp(150);
+		$(this).siblings('ul').slideToggle(150);
+	});
+
+	li.click(function(){
+		var txt = $(this).text();
+		$(this).parent().siblings('.panel').text(txt);
+		$(this).parent().slideUp(150);
+	})
+});
+
+/* Фильтр (селекты) на мобильных устройствах */
+
+$(function(){
+	var link = $('.dop_filters').find('a');
+	var select = $('.filter_block').find('.select');
+
+	link.click(function(){
+		select.toggleClass('showed');
+		return false;
+	})
+
+});
+
+
+
+
+/* Диапазон цен (инициализация) */
+if( $("div").is(".range") ) {
+
+$(function(){
+	var min = 0;
+	var max = 10000;
+
+	$( '#slider-range' ).slider({
+		range: true,
+		min: min,
+		max: max,
+		values: [ min, max ],
+
+		slide: function( event, ui ) {
+			$('#minCost').val(ui.values[ 0 ] + ' р.');
+			$('#maxCost').val(ui.values[ 1 ] + ' р.');
+		}
+		});
+
+	$('#minCost').val($( "#slider-range" ).slider( "values", 0 ) + ' р.');
+	$('#maxCost').val($( "#slider-range" ).slider( "values", 1 ) + ' р.');
+
+	$('#minCost').keyup(function(){
+		min = $(this).val();
+		$( "#slider-range" ).slider({
+			values: [ min, max ]
+		});
+	})
+	$('#maxCost').keyup(function(){
+		max = $(this).val();
+		$( "#slider-range" ).slider({
+			values: [ min, max ]
+		});
+	})
+
+});
+
+}
